d373a132273f9f9f2d71628a498cd973b6d297f8
rayrajovaneli <103432114+rayrajovaneli@users.noreply.github.com>
2022-06-28 06:40:01 -0700

    Initial Commit

---
+++ DidSet/CustomLabelView.swift
@@ -0,0 +1,36 @@
+//
+//  CustomLabelView.swift
+//  DidSet
+//
+//  Created by Rayra Jovaneli on 6/28/22.
+//
+
+import UIKit
+
+class CustomLabelView: UIView {
+
+    var score = "0" {
+        didSet{
+            label.text = score
+        }
+    }
+    
+    private let label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(label)
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 32)
+        label.text = score
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = bounds
+    }
+}
